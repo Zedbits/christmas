eb7f23f1b74529a733789da160b5c972a13b847c
Zed <113933520+Zedbits@users.noreply.github.com>
2022-11-30 13:27:03 -0500

    Update and rename christmas.py to middle.py

--- middle.py
+++ middle.py
@@ -7,17 +7,15 @@ import random
 num_pixels = 30
 np = neopixel.NeoPixel(board.A7, num_pixels, brightness=1, auto_write=False)
 
-red = (255, 0, 0)
-green = (0, 255, 0)
-blue = (0, 0, 255)
-magenta = (255, 0, 255)
-yellow = (255, 255, 0)
-white = (255, 255, 255)
-color = (red, green, blue, magenta, yellow)
-thing  = (red, white)
-
-def middle(hue, stop):
-    color = hue
+"""
+Name: middle
+Description: This function take a color and pulses it from both ends of the LED strip to the 
+middle and then blackens it from the same ends
+Parameters: colors(tuple) - This is the RGB value that will be pulsed. stop(floating point) - this will control how fast each pulse is
+Return: none
+"""
+def middle(colors, stop):
+    color = colors
     for i in range(0, num_pixels/2, 1):
         np[i] = color[i%len(color)]
         np.show()
@@ -32,13 +30,3 @@ def middle(hue, stop):
         np[num_pixels - 1 - i] = (0, 0, 0)
         np.show()
         time.sleep(stop)
-
-    """
-    for i in range(num_pixels):
-        np[i] = color[i%len(color) - 1]
-        np.show()
-        time.sleep(stop)
-    """
-
-while True:
-    middle(color, .01)
